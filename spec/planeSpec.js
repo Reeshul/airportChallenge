df2b95680981186ba56054482ef1242a5f1d2c3f
Reeshul <reeshul@hotmail.com>
2021-02-09 12:35:48 +0000

    passed tests for US1

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -2,10 +2,13 @@
 
 describe("Plane", function () {
   let plane;
+  let airport;
   beforeEach(function () {
-    plane = new plane();
+    plane = new Plane();
+    airport = jasmine.createSpyObj('airport',['clearForLanding']);
   });
   it("can land at an airport", function () {
-    expect(plane.land).not.toBeUndefined();
+    plane.land(airport)
+    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
   });
 });
